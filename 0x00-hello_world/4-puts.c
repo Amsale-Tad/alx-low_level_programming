e8bce79d3f814184a372cfccc6fa869858b9649c
Amsale-Tad <amsalebeykaso@gmail.com>
2022-09-08 15:36:52 -0700

    task 4 and 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
  *after printing the function
  * 
- * Return: 0
+ * Return: Always 0 (success)
  *
  */
 int main(void)
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
 /**
- * main print the string in the put function
+ * main- print the string in the put function
  *
- * description: using the main function
- * program prints "with proper grammar, but the outcome is a piece of art"
- * Return: 0
+ * 
+ * Return: Always 0 (success)
  *
  */
 int main(void)
 {
-	{
- printf("with proper grammar, but the outcome is a piece of art,\n");
-	}
- return(0);
+      printf("with proper grammar, but the outcome is a piece of art,\n");
+      return(0);
 
 }
