fc203963f9b42756f8282b81fe389c2270bcf411
Amsale-Tad <amsalebeykaso@gmail.com>
2022-09-22 13:05:57 -0700

    task 0 and 7

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,16 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
  /** 
   *_strcat - a function that concatenates two strings 
   *@dest: copy to 
   *@src: copy from 
   *Return: two concat strings 
   */
-char *_strcat(char *dest, char *src)
-{
-int index = 0;
-int dest_len = 0;
-while (dest[index++])
-dest_len++;
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index]; return (dest);
+char *_strcat(char *dest, char *src);
+char destination[] = "Hello ";
+char source[] = "World!";
+printf("concentrated srting: %s\n", strcat(destination, source));
+return (0);
 }
+
