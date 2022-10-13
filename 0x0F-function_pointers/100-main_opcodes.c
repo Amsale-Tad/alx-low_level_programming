7a95bf7c24bc6e40a24daa66c508985522ee146b
Amsale-Tad <amsalebeykaso@gmail.com>
2022-10-12 20:25:01 -0700

    task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,28 +9,26 @@
  */
 int main(int argc, char *argv[])
 {
-int bytes, index;
-int (*address)(int, char **) = main;
-unsigned char opcode;
+int x, i;
+unsigned char *p;
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
-bytes = atoi(argv[1]);
-if (bytes < 0)
+x = atoi(argv[1]);
+if (x < 0)
 {
 printf("Error\n");
 exit(2);
 }
-for (index = 0; index < bytes; index++)
+p = (unsigned char *)main;
+i = 0;
+if (x > 0)
 {
-opcode = *(unsigned char *)address;
-printf("%.2x", opcode);
-if (index == bytes - 1)
-continue;
-printf(" ");
-address++;
-printf("\n");
+while (i < (x - 1))
+printf("%02hhx ", p[i++]);
+printf("%hhx\n", p[i]);
+}
 return (0);
 }
