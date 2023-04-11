082eb7317de35bb5df1f6c94abbf96dc93fb3caa
Amsale-Tad <amsalebeykaso@gmail.com>
2023-04-11 22:15:49 +0300

    newest

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,24 +1,28 @@
 #include "search_algos.h"
 
-/**
- * linear_search - implement  linear search  algo
- * @array: given array to search in
- * @size: size of the array
+/** linear_search - searches for a value in an array of integers using Linear
+ * search algorithm
+ * @array: pointer to the first element in the array
+ * @size: number of elements in array
  * @value: value to search for
- * Return: first index or -1
+ *
+ * return: first index where value is located, -1 if value is not present or
+ *array is NULL
  */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
+	int tmp;
 
-	if (array == NULL)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array != NULL)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-
+		for (i = 0; i < size; i++)
+		{
+			tmp = array[i];
+			printf("Value checked array[%lu] = [%d]\n", i, tmp);
+			if (tmp == value)
+				return (i);
+		}
 	}
 	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,41 +1,55 @@
 #include "search_algos.h"
 
 /**
- * binary_search - implement  binary search  algo
- * @array: given array to search in
- * @size: size of the array
+ * print_array - prints an array of integers
+ * @array: array
+ * @size: size
+ *
+ * return: void
+ */
+void print_array(int *array, size_t size)
+{
+	size_t i;
+
+	printf("Searching in array:");
+	for (i = 0; i < size; i++)
+	{
+		printf(" %d", array[i]);
+		if (i != size - 1)
+			printf(",");
+	}
+	printf("\n");
+}
+
+/**
+ * binary_search - searches for a value in a sorted array of integers using the
+ * Binary search algorithm
+ * @array: pointer to the first element of the array
+ * @size: number of elements in array
  * @value: value to search for
- * Return: first index or -1
+ *
+ * return: index where value is located, -1 if fails
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
-	size_t left = 0, right = size - 1, midlle;
+	size_t left, right, middle;
 
-	if (array == NULL)
-		return (-1);
-	while (left <= right)
+	if (array != NULL)
 	{
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
+		left = 0;
+		right = size - 1;
+		print_array(array + left, right + 1 - left);
+		while (left < right)
 		{
-			printf("%i, ", array[i]);
+			middle = (left + right) / 2;
+			if (array[middle] < value)
+				left = middle + 1;
+			else if (array[middle] > value)
+				right = middle;
+			else
+				return (middle);
+			print_array(array + left, right + 1 - left);
 		}
-		printf("%i\n", array[right]);
-		midlle = (left + right) / 2;
-		if (array[midlle] < value)
-		{
-			left = midlle + 1;
-		}
-		else if (array[midlle] > value)
-		{
-			right = midlle - 1;
-		}
-		else
-		{
-			return (midlle);
-		}
-
 	}
 	return (-1);
 }
